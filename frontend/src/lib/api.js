5542189e95aecb735b31b4133269422eb50ec7bb
Cris964 <cristiancaicedo68@gmail.com>
2026-03-11 13:00:17 -0500

    Fix API route path

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://checkadmin-api.onrender.com';
-const api = axios.create({
+const API_URL = 'https://checkadmin-api.onrender.com/api';const api = axios.create({
   baseURL: `${API_URL}`,
   headers: { 'Content-Type': 'application/json' },
 });
